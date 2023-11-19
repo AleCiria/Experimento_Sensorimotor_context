9fde173b060afdf388f924e5c050cf12e7208eb0
AleCiria <ciriacontacto@gmail.com>
2023-11-19 15:51:17 -0600

    Add files via upload

---
+++ stimuli_1.js
@@ -0,0 +1,470 @@
+var stimuli_1 = [
+
+{
+sentence: "Juan se relajó jugando una ronda de",
+word: "ajedrez",
+type: "A",
+sim: "0.6",
+fam: "4.746",
+imag: "5.318",
+conc: "5.804"
+},
+
+{
+sentence: "En el jardín había un pequeño",
+word: "árbol",
+type: "A",
+sim: "0.87",
+fam: "6.678",
+imag: "6.292",
+conc: "5.921"
+},
+
+{
+sentence: "En el centro comercial vi a un",
+word: "amigo",
+type: "A",
+sim: "0.44",
+fam: "6.781",
+imag: "6.175",
+conc: "5.853"
+},
+
+{
+sentence: "La constructora contrató a un nuevo",
+word: "arquitecto",
+type: "A",
+sim: "0.4",
+fam: "4.724",
+imag: "4.617",
+conc: "5.959"
+},
+
+{
+sentence: "Cuando caminaba por la calle me encontré un",
+word: "billete",
+type: "A",
+sim: "0.71",
+fam: "6.514",
+imag: "6.514",
+conc: "4.933"
+},
+
+{
+sentence: "En la tienda departamental, vi una",
+word: "blusa",
+type: "A",
+sim: "0.63",
+fam: "5.681",
+imag: "5.195",
+conc: "6.400"
+},
+
+{
+sentence: "Como salió muy apurada, olvidó llevar su",
+word: "bolsa",
+type: "A",
+sim: "0.57",
+fam: "6.416",
+imag: "5.035",
+conc: "4.724"
+},
+
+{
+sentence: "En el cajón de la bodega, encontré una",
+word: "caja",
+type: "A",
+sim: "0.76",
+fam: "6.533",
+imag: "6.232",
+conc: "4.581"
+},
+
+{
+sentence: "El médico le aconsejó mudarse al",
+word: "campo",
+type: "A",
+sim: "0.45",
+fam: "6.334",
+imag: "5.963",
+conc: "4.773"
+},
+
+{
+sentence: "El técnico ajustó las luces del",
+word: "carro",
+type: "A",
+sim: "0.62",
+fam: "5.480",
+imag: "5.966",
+conc: "5.565"
+},
+
+{
+sentence: "María necesitaba un técnico para arreglar la",
+word: "computadora",
+type: "A",
+sim: "0.74",
+fam: "4.545",
+imag: "5.880",
+conc: "5.467"
+},
+
+{
+sentence: "Mi hermano fue al médico porque tiene una",
+word: "enfermedad",
+type: "A",
+sim: "0.74",
+fam: "6.468",
+imag: "4.962",
+conc: "3.555"
+},
+
+{
+sentence: "Como plato fuerte, pidió una",
+word: "ensalada",
+type: "A",
+sim: "0.592",
+fam: "6.575",
+imag: "6.146",
+conc: "5.114"
+},
+
+{
+sentence: "Me sentí muy cansada después de la",
+word: "escuela",
+type: "A",
+sim: "0.85",
+fam: "5.648",
+imag: "6.005",
+conc: "5.832"
+},
+
+{
+sentence: "La señora encendió en su casa la",
+word: "estufa",
+type: "A",
+sim: "0.42",
+fam: "6.332",
+imag: "5.992",
+conc: "6.162"
+},
+
+{
+sentence: "Al lado del lago, hubo una gran",
+word: "fiesta",
+type: "A",
+sim: "0.75",
+fam: "6.750",
+imag: "6.273",
+conc: "4.698"
+},
+
+{
+sentence: "Todos los veranos, los agricultores vendían mucha",
+word: "fruta",
+type: "A",
+sim: "0.54",
+fam: "6.330",
+imag: "5.333",
+conc: "4.911"
+},
+
+{
+sentence: "Mi mamá compró, en las artesanías, un",
+word: "jarrón",
+type: "A",
+sim: "0.43",
+fam: "5.378",
+imag: "6.350",
+conc: "6.249"
+},
+
+{
+sentence: "En el escritorio, no encontré mi",
+word: "lápiz",
+type: "A",
+sim: "0.59",
+fam: "6.121",
+imag: "5.960",
+conc: "6.765"
+},
+
+{
+sentence: "La maestra pidió a los niños un",
+word: "libro",
+type: "A",
+sim: "0.66",
+fam: "6.857",
+imag: "6.261",
+conc: "6.066"
+},
+
+{
+sentence: "María le habló por teléfono a su",
+word: "mamá",
+type: "A",
+sim: "0.59",
+fam: "6.598",
+imag: "6.498",
+conc: "6.470"
+},
+
+{
+sentence: "En el concierto de ayer, hubo una gran",
+word: "multitud",
+type: "A",
+sim: "0.77",
+fam: "6.678",
+imag: "6.326",
+conc: "5.279"
+},
+
+
+{
+sentence: "El niño pequeño tenía una muy mala",
+word: "ortografía",
+type: "A",
+sim: "0.64",
+fam: "4.711",
+imag: "4.238",
+conc: "5.703"
+},
+
+{
+sentence: "Pepe juega todo el día en el",
+word: "parque",
+type: "A",
+sim: "0.74",
+fam: "6.311",
+imag: "6.557",
+conc: "6.057" 
+},
+
+{
+sentence: "La lluvia de anoche mojó mucho el",
+word: "patio",
+type: "A",
+sim: "0.51",
+fam: "5.898",
+imag: "6.772",
+conc: "6.091"
+},
+
+{
+sentence: "Ella no pudo identificar el origen del",
+word: "problema",
+type: "A",
+sim: "0.72",
+fam: "6.519",
+imag: "4.017",
+conc: "2.937"
+},
+
+{
+sentence: "Mi padre me dio, por la tarde, un",
+word: "regalo",
+type: "A",
+sim: "0.8",
+fam: "5.974",
+imag: "6.030",
+conc: "4.788"
+},
+
+{
+sentence: "Pudo conseguir dinero vendiendo la",
+word: "ropa",
+type: "A",
+sim: "0.6",
+fam: "6.613",
+imag: "6.218",
+conc: "3.791"
+},
+
+{
+sentence: "En la montaña se podía escuchar un",
+word: "ruido",
+type: "A",
+sim: "0.67",
+fam: "6.392",
+imag: "5.335",
+conc: "4.037"
+},
+
+{
+sentence: "La pequeña banda rentó un",
+word: "salón",
+type: "A",
+sim: "0.25",
+fam: "6.444",
+imag: "6.427",
+conc: "4.780"
+},
+
+{
+sentence: "Juan no pudo encender la",
+word: "televisión",
+type: "A",
+sim: "0.62",
+fam: "6.420",
+imag: "6.377",
+conc: "6.235"
+},
+
+{
+sentence: "Vi pastando en la granja a un",
+word: "caballo",
+type: "A",
+sim: "0.78",
+fam: "5.596",
+imag: "6.648",
+conc: "6.228"
+},
+
+{
+sentence: "El buró de mi cuarto tiene un",
+word: "cajón",
+type: "A",
+sim: "0.54",
+fam: "6.635",
+imag: "6.392",
+conc: "6.101"
+},
+
+{
+sentence: "El mesero recomendó la pasta y la",
+word: "carne",
+type: "A",
+sim: "0.6",
+fam: "6.506",
+imag: "6.228",
+conc: "5.643"
+},
+
+{
+sentence: "Pudo conseguir dinero vendiendo la",
+word: "casa",
+type: "A",
+sim: "0.62",
+fam: "6.759",
+imag: "6.575",
+conc: "5.631"
+},
+
+{
+sentence: "Pensé que comíamos pollo, pero en realidad era",
+word: "conejo",
+type: "A",
+sim: "0.68",
+fam: "6.029",
+imag: "6.061",
+conc: "6.365"
+},
+
+{
+sentence: "Se pasó la tarde cosiendo con un",
+word: "hilo",
+type: "A",
+sim: "0.68",
+fam: "6.613",
+imag: "6.370",
+conc: "5.825"
+},
+
+{
+sentence: "El hombre tuvo una nueva",
+word: "idea",
+type: "A",
+sim: "0.7",
+fam: "6.749",
+imag: "3.976",
+conc: "4.078"
+},
+
+{
+sentence: "En la casa del vecino, parecía que había un",
+word: "ladrón",
+type: "A",
+sim: "0.76",
+fam: "6.307",
+imag: "5.438",
+conc: "5.524"
+},
+
+{
+sentence: "En su presentación, María vio a la",
+word: "maestra",
+type: "A",
+sim: "0.69",
+fam: "5.984",
+imag: "5.787",
+conc: "5.845"
+},
+
+{
+sentence: "Para el viaje, mi papá llevó una",
+word: "maleta",
+type: "A",
+sim: "0.54",
+fam: "5.838",
+imag: "4.870",
+conc: "6.120"
+},
+
+{
+sentence: "En la tarde, mi papá se aventó al",
+word: "mar",
+type: "A",
+sim: "0.48",
+fam: "6.545",
+imag: "6.050",
+conc: "6.293"
+},
+
+{
+sentence: "En la historia, el lobo persiguió al",
+word: "niño",
+type: "A",
+sim: "0.69",
+fam: "6.513",
+imag: "6.390",
+conc: "6.177"
+},
+
+{
+sentence: "Por la noche, en el bosque, encontré un",
+word: "oso",
+type: "A",
+sim: "0.74",
+fam: "5.517",
+imag: "5.342",
+conc: "6.383"
+},
+
+{
+sentence: "El señor ya había localizado a su",
+word: "perro",
+type: "A",
+sim: "0.73",
+fam: "6.639",
+imag: "6.573",
+conc: "6.443"
+},
+
+{
+sentence: "Después del viaje, cambiaron la pila del viejo",
+word: "reloj",
+type: "A",
+sim: "0.65",
+fam: "6.768",
+imag: "6.043",
+conc: "6.518"
+},
+
+]
+
+
+
+
+
+
---
+++ stimuli_2.js
@@ -0,0 +1,464 @@
+var stimuli_2 = [
+
+{
+sentence: "En la escuela, encontré una pieza de",
+word: "ajedrez",
+type: "B",
+sim: "0.6",
+fam: "4.746",
+imag: "5.318",
+conc: "5.804"
+},
+
+{
+sentence: "Ayer por la tarde, Mario se cayó del",
+word: "árbol",
+type: "B",
+sim: "0.87",
+fam: "6.678",
+imag: "6.292",
+conc: "5.921"
+},
+
+{
+sentence: "Los niños sufren con la pérdida de un",
+word: "amigo",
+type: "B",
+sim: "0.44",
+fam: "6.781",
+imag: "6.175",
+conc: "5.853"
+},
+
+{
+sentence: "El joven estudió para ser un gran",
+word: "arquitecto",
+type: "B",
+sim: "0.4",
+fam: "4.724",
+imag: "4.617",
+conc: "5.959"
+},
+
+{
+sentence: "La señora le dio al empacador un",
+word: "billete",
+type: "B",
+sim: "0.71",
+fam: "6.514",
+imag: "6.514",
+conc: "4.933"
+},
+
+{
+sentence: "Mi mamá se compró una nueva",
+word: "blusa",
+type: "B",
+sim: "0.63",
+fam: "5.681",
+imag: "5.195",
+conc: "6.400"
+},
+
+{
+sentence: "Creo que esto deberías guardarlo en tu",
+word: "bolsa",
+type: "B",
+sim: "0.57",
+fam: "6.416",
+imag: "5.035",
+conc: "4.724"
+},
+
+{
+sentence: "En la alacena, tenemos una",
+word: "caja",
+type: "B",
+sim: "0.76",
+fam: "6.533",
+imag: "6.232",
+conc: "4.581"
+},
+
+{
+sentence: "Para descansar, la familia fue al",
+word: "campo",
+type: "B",
+sim: "0.45",
+fam: "6.334",
+imag: "5.963",
+conc: "4.773"
+},
+
+{
+sentence: "Cuando mi novia se mudó, vendió el",
+word: "carro",
+type: "B",
+sim: "0.62",
+fam: "5.480",
+imag: "5.966",
+conc: "5.565"
+},
+
+{
+sentence: "En la mudanza, se dañó mi",
+word: "computadora",
+type: "B",
+sim: "0.74",
+fam: "4.545",
+imag: "5.880",
+conc: "5.467"
+},
+
+{
+sentence: "La celebridad mostró signos de una",
+word: "enfermedad",
+type: "B",
+sim: "0.74",
+fam: "6.468",
+imag: "4.962",
+conc: "3.555"
+},
+
+{
+sentence: "Para el almuerzo, ella hizo una deliciosa",
+word: "ensalada",
+type: "B",
+sim: "0.592",
+fam: "6.575",
+imag: "6.146",
+conc: "5.114"
+},
+
+{
+sentence: "A mi hermano le gusta estudiar en la",
+word: "escuela",
+type: "B",
+sim: "0.85",
+fam: "5.648",
+imag: "6.005",
+conc: "5.832"
+},
+
+{
+sentence: "Se supone que los niños no deben tocar la",
+word: "estufa",
+type: "B",
+sim: "0.42",
+fam: "6.332",
+imag: "5.992",
+conc: "6.162"
+},
+
+{
+sentence: "Él gastó mucho dinero en la",
+word: "fiesta",
+type: "B",
+sim: "0.75",
+fam: "6.750",
+imag: "6.273",
+conc: "4.698"
+},
+
+{
+sentence: "Mi mamá fue al mercado a comprar",
+word: "fruta",
+type: "B",
+sim: "0.54",
+fam: "6.330",
+imag: "5.333",
+conc: "4.911"
+},
+
+{
+sentence: "La señora de la limpieza rompió el",
+word: "jarrón",
+type: "B",
+sim: "0.43",
+fam: "5.378",
+imag: "6.350",
+conc: "6.249"
+},
+
+{
+sentence: "La niña le pidió a su profesor un",
+word: "lápiz",
+type: "B",
+sim: "0.59",
+fam: "6.121",
+imag: "5.960",
+conc: "6.765"
+},
+
+{
+sentence: "Hugo siempre tenía listo un",
+word: "libro",
+type: "B",
+sim: "0.66",
+fam: "6.857",
+imag: "6.261",
+conc: "6.066"
+},
+
+{
+sentence: "Fui a los juegos acompañado de mi",
+word: "mamá",
+type: "B",
+sim: "0.59",
+fam: "6.598",
+imag: "6.498",
+conc: "6.470"
+},
+
+{
+sentence: "Ella se presentó ante una gran",
+word: "multitud",
+type: "B",
+sim: "0.77",
+fam: "6.678",
+imag: "6.326",
+conc: "5.279"
+},
+
+{
+sentence: "El profesor le ayudó a mejorar su",
+word: "ortografía",
+type: "B",
+sim: "0.64",
+fam: "4.711",
+imag: "4.238",
+conc: "5.703"
+},
+
+{
+sentence: "A Elizabeth no le gustaba ir al",
+word: "parque",
+type: "B",
+sim: "0.74",
+fam: "6.311",
+imag: "6.557",
+conc: "6.057"
+},
+
+{
+sentence: "Vimos a nuestro vecino trabajando en el",
+word: "patio",
+type: "B",
+sim: "0.51",
+fam: "5.898",
+imag: "6.772",
+conc: "6.091"
+},
+
+{
+sentence: "Le preguntaron al vecino cuál era su",
+word: "problema",
+type: "B",
+sim: "0.72",
+fam: "6.519",
+imag: "4.017",
+conc: "2.937"
+},
+
+{
+sentence: "Me dijo que costaría caro dar un buen",
+word: "regalo",
+type: "B",
+sim: "0.8",
+fam: "5.974",
+imag: "6.030",
+conc: "4.788"
+},
+
+{
+sentence: "En la mañana, lavé cuidadosamente mi",
+word: "ropa",
+type: "B",
+sim: "0.6",
+fam: "6.613",
+imag: "6.218",
+conc: "3.791"
+},
+
+{
+sentence: "Afuera de la casa, había mucho",
+word: "ruido",
+type: "B",
+sim: "0.67",
+fam: "6.392",
+imag: "5.335",
+conc: "4.037"
+},
+
+{
+sentence: "La fiesta fue en un",
+word: "salón",
+type: "B",
+sim: "0.25",
+fam: "6.444",
+imag: "6.427",
+conc: "4.780"
+},
+
+{
+sentence: "Él fue a comprar un soporte para su",
+word: "televisión",
+type: "B",
+sim: "0.62",
+fam: "6.420",
+imag: "6.377",
+conc: "6.235"
+},
+
+{
+sentence: "El granjero compró una vaca y un",
+word: "caballo",
+type: "B",
+sim: "0.78",
+fam: "5.596",
+imag: "6.648",
+conc: "6.228"
+},
+
+{
+sentence: "Guardaron todos los adornos de Navidad en el",
+word: "cajón",
+type: "B",
+sim: "0.54",
+fam: "6.635",
+imag: "6.392",
+conc: "6.101"
+},
+
+{
+sentence: "Necesitaba un cuchillo para rebanar la",
+word: "carne",
+type: "B",
+sim: "0.6",
+fam: "6.506",
+imag: "6.228",
+conc: "5.643"
+},
+
+{
+sentence: "Mi hermano buscaba algo en la",
+word: "casa",
+type: "B",
+sim: "0.62",
+fam: "6.759",
+imag: "6.575",
+conc: "5.631"
+},
+
+{
+sentence: "El cazador logró atrapar un",
+word: "conejo",
+type: "B",
+sim: "0.68",
+fam: "6.029",
+imag: "6.061",
+conc: "6.365"
+},
+
+{
+sentence: "Fui a la mercería a comprar",
+word: "hilo",
+type: "B",
+sim: "0.68",
+fam: "6.613",
+imag: "6.370",
+conc: "5.825"
+},
+
+
+{
+sentence: "Tomás les pidió que pensaran en una",
+word: "idea",
+type: "B",
+sim: "0.7",
+fam: "6.749",
+imag: "3.976",
+conc: "4.078"
+},
+
+{
+sentence: "La semana pasada metieron a la cárcel al",
+word: "ladrón",
+type: "B",
+sim: "0.76",
+fam: "6.307",
+imag: "5.438",
+conc: "5.524"
+},
+
+{
+sentence: "Al llegar, Luis saludó a la",
+word: "maestra",
+type: "B",
+sim: "0.69",
+fam: "5.984",
+imag: "5.787",
+conc: "5.845"
+},
+
+{
+sentence: "En el aeropuerto, la aeromoza tomó su",
+word: "maleta",
+type: "B",
+sim: "0.54",
+fam: "5.838",
+imag: "4.870",
+conc: "6.120"
+},
+
+{
+sentence: "La mejor comida es la que viene del",
+word: "mar",
+type: "B",
+sim: "0.48",
+fam: "6.545",
+imag: "6.050",
+conc: "6.293"
+},
+
+{
+sentence: "Luis es más alto que el otro",
+word: "niño",
+type: "B",
+sim: "0.69",
+fam: "6.513",
+imag: "6.390",
+conc: "6.177"
+},
+
+{
+sentence: "El cazador disparó y mató a un gran",
+word: "oso",
+type: "B",
+sim: "0.74",
+fam: "5.517",
+imag: "5.342",
+conc: "6.383"
+},
+
+{
+sentence: "Paco le abrió la puerta al",
+word: "perro",
+type: "B",
+sim: "0.73",
+fam: "6.639",
+imag: "6.573",
+conc: "6.443"
+},
+
+{
+sentence: "El coleccionista decidió comprar un costoso",
+word: "reloj",
+type: "B",
+sim: "0.65",
+fam: "6.768",
+imag: "6.043",
+conc: "6.518"
+}
+
+]
